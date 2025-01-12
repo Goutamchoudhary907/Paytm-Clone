25a3ecd3544140548b0285cd56330a76af00a809
Goutam Choudhary <goutamchoudhary907@gmail.com>
2025-01-12 19:48:58 +0530

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express=require('express');
 const cors=require("cors")
 const app=express()
-app.use(cors())
+app.use(cors(
+    {
+        origin:["https://paytm-clone-frontendd.vercel.app/"],
+        methods:["POST","GET"] ,
+        credentials:true
+    }
+))
 app.use(express.json()) 
 const mainRouter=require('./routes/index')
 
